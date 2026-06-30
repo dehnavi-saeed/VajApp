16bda71ed11bd8db480d3a442260afb754a7c768
Saeed <Saeed@DESKTOP-HO881VD>
2026-06-30 23:28:16 +0330

    Currect Error

--- download/vaj/backend/src/main/java/app/vaj/book/domain/ReadingStatus.java
+++ download/vaj/backend/src/main/java/app/vaj/book/domain/ReadingStatus.java
@@ -3,7 +3,3 @@ package app.vaj.book.domain;
 public enum ReadingStatus {
     DRAFT, UNREAD, READING, COMPLETED, ARCHIVED, DELETED
 }
-
-public enum BookFormat {
-    PHYSICAL, EBOOK, AUDIOBOOK, PDF
-}
---
+++ download/vaj/backend/src/main/java/app/vaj/common/package-info.java
@@ -0,0 +1,6 @@
+@ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package app.vaj.common;
+
+import org.springframework.modulith.ApplicationModule;
